21410bc076bae11296d84315983b86c8ae6178cc
Rahul Yadav <rahuly247@gmail.com>
2016-12-06 00:37:04 +0530

    Initial Commit

---
+++ desktop/src/com/sdsmdg/flingit/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sdsmdg.flingit.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sdsmdg.flingit.FLINGitGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FLINGitGame(), config);
+	}
+}
