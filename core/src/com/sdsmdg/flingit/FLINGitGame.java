739696fc6dfb5b20914ed0e313eb9a38d5b2d50c
Rahul Yadav <rahuly247@gmail.com>
2016-12-06 02:38:53 +0530

    Added PlayScreen

--- core/src/com/sdsmdg/flingit/FLINGitGame.java
+++ core/src/com/sdsmdg/flingit/FLINGitGame.java
@@ -1,33 +1,28 @@
 package com.sdsmdg.flingit;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.sdsmdg.flingit.Screens.PlayScreen;
 
-public class FLINGitGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class FLINGitGame extends Game {
+
+	public SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		//It delegates the action to the main screen or the screen which is active
+		//at that point of time
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/com/sdsmdg/flingit/Screens/PlayScreen.java
@@ -0,0 +1,61 @@
+package com.sdsmdg.flingit.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.sdsmdg.flingit.FLINGitGame;
+
+/**
+ * Created by rahul on 6/12/16.
+ */
+
+public class PlayScreen implements Screen {
+
+    private FLINGitGame game;
+    Texture texture;
+
+    public PlayScreen(FLINGitGame game) {
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(texture, 0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        game.batch.dispose();
+    }
+}
