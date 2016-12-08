7d35222179a2bc328a0cc0094f8c2bd5f058eae3
Rahul Yadav <rahuly247@gmail.com>
2016-12-08 18:59:11 +0530

    Added small block on the top of main block

--- core/src/com/sdsmdg/flingit/constants/Constants.java
+++ core/src/com/sdsmdg/flingit/constants/Constants.java
@@ -1,9 +1,26 @@
 package com.sdsmdg.flingit.constants;
 
+import com.badlogic.gdx.graphics.Color;
+
 /**
  * Created by rahul on 7/12/16.
  */
 
 public class Constants {
     public static final int BLOCK_COUNT = 3;
+
+    public static final float RADIUS_FACTOR = 20;
+    public static final Color COLOR_BODY_FADED = toRGB(245, 0, 87);
+    public static final Color COLOR_BODY = Color.PINK;
+    public static final Color COLOR_BOTTOM = Color.OLIVE;
+    public static final Color COLOR_TOP = Color.GOLD;
+
+
+    private static Color toRGB(int r, int g, int b) {
+        float RED = r / 255.0f;
+        float GREEN = g / 255.0f;
+        float BLUE = b / 255.0f;
+        return new Color(RED, GREEN, BLUE, 1);
+    }
+
 }
