7bf26afd7a109a0eea7915349b705acf733ee780
Rahul Yadav <rahuly247@gmail.com>
2016-12-09 23:20:14 +0530

    Added Game Over function if ball crosses Game World

--- core/src/com/sdsmdg/flingit/sprites/Body.java
+++ core/src/com/sdsmdg/flingit/sprites/Body.java
@@ -91,8 +91,8 @@ public class Body extends InputAdapter {
             velocity.x = 0;
             game.setScreen(new PlayScreen(game));
         }
-        if (position.x + radius > viewportWidth) {
-
+        if (position.x + 3 * radius > viewportWidth) {
+            game.setScreen(new PlayScreen(game));
         }
         if (position.y - radius < 0) {
             position.y = radius;
