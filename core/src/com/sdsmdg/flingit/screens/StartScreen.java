0e8079bf707bba728eaf0a193d590380165d30a2
Rahul Yadav <rahuly247@gmail.com>
2016-12-11 00:38:49 +0530

    Added StartScreen.java

---
+++ core/src/com/sdsmdg/flingit/screens/StartScreen.java
@@ -0,0 +1,48 @@
+package com.sdsmdg.flingit.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by rahul on 11/12/16.
+ */
+
+public class StartScreen implements Screen {
+
+    public StartScreen() {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
